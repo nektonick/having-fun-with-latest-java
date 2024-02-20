d4a4152f12deb5f2d6e6f33a6345b25b31572563
nektonick <nik-1905@mail.ru>
2024-02-21 02:05:54 +0300

    Add StreamApiSamples.joinMultipleLists()

--- src/main/java/org/example/streamapi/StreamApiSamples.java
+++ src/main/java/org/example/streamapi/StreamApiSamples.java
@@ -1,5 +1,7 @@
 package org.example.streamapi;
 
+import lombok.val;
+
 import java.util.List;
 import java.util.stream.Stream;
 
@@ -9,5 +11,18 @@ public final class StreamApiSamples {
 
     public static <T> List<T> joinLists(List<T> firstList, List<T> secondList) {
         return Stream.concat(firstList.stream(), secondList.stream()).toList();
+    }
+
+    @SafeVarargs
+    public static <T> List<T> joinMultipleLists(List<T> firstList, List<T>... otherLists) {
+        if (otherLists.length == 0) {
+            return firstList;
+        }
+
+        var finalStream = firstList.stream();
+        for (val otherList : otherLists) {
+            finalStream = Stream.concat(finalStream, otherList.stream());
+        }
+        return finalStream.toList();
     }
 }
